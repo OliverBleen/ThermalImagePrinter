dbdc012e34913d02e82bc53ac04c55f4e6ab78dd
Oliver Bleen <oliverbleen@noreply.localhost>
2026-07-06 21:08:18 +0200

    Compare GUIDs using .ToString() Without it the query always returns false

--- ImageAPI/ImageAPI/Models/Database/DatabaseHelper.cs
+++ ImageAPI/ImageAPI/Models/Database/DatabaseHelper.cs
@@ -13,12 +13,12 @@ class DatabaseHelper
     public static async Task<bool> ApiKeyExists(Guid apiKey)
     {
         using var _context = new DatabaseContext();
-        return await _context.ApiKeys.AnyAsync(k => k.Key == apiKey && k.Active != 0);
+        return await _context.ApiKeys.AnyAsync(k => k.Key.ToString() == apiKey.ToString() && k.Active != 0);
     }
     public static async Task<bool> ApiKeyHasAccess(Guid apiKey, string section)
     {
         using var _context = new DatabaseContext();
-        var key = await _context.ApiKeys.FirstAsync(k => k.Key == apiKey && k.Active != 0);
+        var key = await _context.ApiKeys.FirstAsync(k => k.Key.ToString() == apiKey.ToString() && k.Active != 0);
 
         if(key.AccessTo == "*") //Special case for master / testing key
             return true;
--- ImageAPI/ImageAPI/Program.cs
+++ ImageAPI/ImageAPI/Program.cs
@@ -12,7 +12,7 @@ namespace ImageAPI;
 
 public class Program
 {
-    public static readonly string API_VERSION = "v1.6.1";
+    public static readonly string API_VERSION = "v1.6.2";
     // => optionsBuilder.UseSqlite($"Filename={Path.Combine(Program.BASE_DIR, "Database.sqlite3")}");
     public static string BASE_DIR = "./bin/Debug/";
 
