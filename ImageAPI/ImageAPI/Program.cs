15b996dc7f989460f3f9e1ba119c585dd59623d5
Oliver Bleen <oliverbleen@noreply.localhost>
2026-07-10 13:18:21 +0200

    Change listening address

--- ImageAPI/ImageAPI/Program.cs
+++ ImageAPI/ImageAPI/Program.cs
@@ -14,7 +14,7 @@ namespace ImageAPI;
 
 public class Program
 {
-    public static readonly string API_VERSION = "v1.7.4";
+    public static readonly string API_VERSION = "v1.7.5";
     // => optionsBuilder.UseSqlite($"Filename={Path.Combine(Program.BASE_DIR, "Database.sqlite3")}");
     public static string BASE_DIR = "./bin/Debug/";
 
