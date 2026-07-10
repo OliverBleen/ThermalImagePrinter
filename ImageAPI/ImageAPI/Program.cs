872804b42c739f5c327e8d53156edaa1a58d8c03
Oliver Bleen <oliverbleen@noreply.localhost>
2026-07-10 09:50:29 +0200

    Disable Information logging for ASP.NET

--- ImageAPI/ImageAPI/Program.cs
+++ ImageAPI/ImageAPI/Program.cs
@@ -13,7 +13,7 @@ namespace ImageAPI;
 
 public class Program
 {
-    public static readonly string API_VERSION = "v1.7.0";
+    public static readonly string API_VERSION = "v1.7.1";
     // => optionsBuilder.UseSqlite($"Filename={Path.Combine(Program.BASE_DIR, "Database.sqlite3")}");
     public static string BASE_DIR = "./bin/Debug/";
 
